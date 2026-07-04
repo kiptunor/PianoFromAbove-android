2e6e39a8c08a40bfad4690e190cfd4a28e2115bc
kiptunor <andreicu07@gmail.com>
2026-07-05 01:42:04 +0300

    Getting android build working

---
+++ src/android/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.qsp.nvpfa
+
+import android.os.Bundle
+import org.libsdl.app.SDLActivity
+
+class MainActivity : SDLActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Request permissions
+        // Initialize your app
+    }
+}
