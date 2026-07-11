37265db86ac706ffc84ef8272f9d671d3a41c5b5
kiptunor <andreicu07@gmail.com>
2026-07-12 02:01:01 +0300

    [xmake] Added target name on android build logs

--- apk_build_utils/javac.lua
+++ apk_build_utils/javac.lua
@@ -1,7 +1,8 @@
 function main(opts)
+    local target    = opts.target
     local args = {"-d", opts.output, "-cp", opts.classpath}
     for _, src in ipairs(opts.sources) do
-        cprint("${green}[Java]:${clear} compiling: %s", opts.sources)
+        cprint("${green}[Java]:${clear}        ${bright magenta}<%s>${clear} Compiling: %s", target:name(), opts.sources)
         table.insert(args, src)
     end
     os.vrunv("javac", args)
